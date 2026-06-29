54b4f29f432156e66bdfa39bca628fd18b54a28d
pichuu-afk <134182414+pichuu-afk@users.noreply.github.com>
2026-06-29 12:05:11 +0800

    Update project link to GitHub Pages

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -31,7 +31,7 @@ const PROJECTS = [
     tags: ["HTML/CSS", "JavaScript", "SEO"],
     features: [],
     featured: false,
-    href: "https://crilinesacademy.com",
+    href: "https://crilinesacademy.github.io",
   },
 ];
 
